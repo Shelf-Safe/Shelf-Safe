d3d10164a91202799d6a3db46979889e92165f3e
Cloud 9 <anmollangara@gmail.com>
2026-03-14 01:02:04 -0700

    SHELFSAFE-166 few changes for live deployment testing

--- backend/serverLive.js
+++ backend/serverLive.js
@@ -33,7 +33,6 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
-
 app.use(express.json({ limit: '20mb' }));
 app.use(express.urlencoded({ extended: true, limit: '20mb' }));
 
